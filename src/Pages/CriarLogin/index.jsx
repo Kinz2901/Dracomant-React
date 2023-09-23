827f503b3878f48b513a84e5aa6b2aba39841ce5
Guilherme Lins <guilhermelins978@gmail.com>
2023-09-23 01:41:50 -0300

    coloquei os tipos de erro no criar login

--- src/Pages/CriarLogin/index.jsx
+++ src/Pages/CriarLogin/index.jsx
@@ -10,8 +10,40 @@ export default function CriarLogin() {
 
   const [ name, setName ] = useState("")
   const [ email, setEmail ] = useState("")
-  const [ passwords, setPassword ] = useState("")
-  const [ passwordsConfirm, setPasswordConfirm ] = useState("")
+  const [ password, setPassword ] = useState("")
+  const [ passwordConfirm, setPasswordConfirm ] = useState("")
+
+  const [ errEmail,  setErrEmail ] = useState("")
+  const [ errPassword,  setErrSenha ] = useState(false)
+  const [ errPasswordConfirm,  setErrSenhaConfirm ] = useState(false)
+
+
+  
+  const validateEmail = (ev) => {
+    ev.preventDefault()
+      setErrEmail("")
+      setErrSenha("")
+      setErrSenhaConfirm("")
+
+        if (!email.match(/\w{2,}@[a-zA-Z]{2,}\.[a-zA-Z]{2,}/)) {
+          setErrEmail("E-mail inválido.")
+        } 
+        if (password.length < 8 ) {
+          setErrSenha("O tamanho da sua senha deve ser de no minimo 8 digitos.")
+        }
+        if (!password.match(/[a-z]/)) {
+          setErrSenha("Sua senha deve conter letras minúsculas.")
+        }
+        if (!password.match(/[A-Z]/)) {
+          setErrSenha("Sua senha deve conter letras maiúsculas.")
+        }
+        if (!password.match(/[0-9]/)) {
+          setErrSenha("Sua senha deve conter números.")
+        }
+        if (passwordConfirm !== password) {
+          setErrSenhaConfirm("As senhas não se coincidem.")
+        }
+  }   
 
   return (
     <>
@@ -19,11 +51,10 @@ export default function CriarLogin() {
         <div className={styles.bloco}>
           <h2 className={styles.titulo}>DRACOMANT</h2>
           <p className={styles.criarConta}>CRIE UMA CONTA</p>
-          <form className={styles.form}>
+          <form onSubmit={validateEmail} className={styles.form}>
             <div className={styles.blocos_input} id="bloco-nome">
               <input
-                onChange={(ev) => setName(ev.target)}
-                value={name}
+                onChange={(ev) => setName(ev.target.value)}
                 className={styles.nome}
                 id="name"
                 type="text"
@@ -35,6 +66,7 @@ export default function CriarLogin() {
             <span className={styles.err} id="name-error"></span>
             <div className={styles.blocos_input} id="bloco-email">
               <input
+                onChange={(ev) => setEmail(ev.target.value)}
                 className={styles.email}
                 id="email"
                 type="email"
@@ -43,9 +75,10 @@ export default function CriarLogin() {
               />
               <AiOutlineMail className={styles.icons} />
             </div>
-            <span className={styles.err} id="email-error"></span>
+            <span className={styles.err} id="email-error">{errEmail}</span> 
             <div className={styles.blocos_input} id="bloco-password">
               <input
+                onChange={(ev) => setPassword(ev.target.value)}
                 className={styles.password}
                 id="password"
                 type={visiblePass ? "text" : "password"}
@@ -64,9 +97,10 @@ export default function CriarLogin() {
                 />
               )}
             </div>
-            <span className={styles.err} id="password-error"></span>
+            <span className={styles.err}>{errPassword}</span>
             <div className={styles.blocos_input} id="bloco-passwordConfirm">
               <input
+                onChange={(ev) => setPasswordConfirm(ev.target.value)}
                 className={styles.passwordConfirm}
                 id="passwordConfirm"
                 type={visiblePassConfirm ? "text" : "password"}
@@ -85,8 +119,8 @@ export default function CriarLogin() {
                 />
               )}
             </div>
-            <span className={styles.err} id="passwordConfirm-error"></span>
-            <button className={styles.botao}>
+            <span className={styles.err}>{errPasswordConfirm}</span>
+            <button  onClick={() => validateEmail} className={styles.botao}>
               CRIAR CONTA
             </button>
             <p className={styles.possuiConta}>
