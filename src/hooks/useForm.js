f7a985955570872e5a65768072167ca4d33c4b02
Guilherme Lins <guilhermelins978@gmail.com>
2024-01-04 15:43:10 -0300

    diminui a regex de password e modifiquei as mensagens de erro

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -4,11 +4,11 @@ const types = {
   email: {
     regex:
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    message: "Email inválido",
+    message: "Preencha um email inválido",
   },
   password: {
-    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/,
-    message: "Senha inválida",
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    message: "A senha precisa ter um caracter maiúsculo, 1 minúsculo e 1 digito.Com no mínimo 8 caracateres.",
   }
 };
 
@@ -17,8 +17,9 @@ export default function useForm(type) {
   const [error, setError] = useState(null);
   
   function validate(value) {
+    if (type === false) return true
     if (value.length === 0) {
-      setError("Preencha o campo");
+      setError("Preencha um valor.");
       return false;
     } else if (types[type] && !types[type].regex.test(value)) {
       setError(types[type].message);
