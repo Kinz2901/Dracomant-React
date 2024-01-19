a754a1a179f393d87192af969a334a1e6f955b97
Guilherme Lins <guilhermelins978@gmail.com>
2024-01-19 18:50:01 -0300

    s
    s

--- src/Pages/Conta/Conta.jsx
+++ src/Pages/Conta/Conta.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import styles from "./index.module.css";
-import { FaUserCircle } from "react-icons/fa";
-import { FaCircleUser, FaRegCircleUser } from "react-icons/fa6";
+import { FaCircleUser } from "react-icons/fa6";
+import { IoExitOutline } from "react-icons/io5";
+import { MdModeEditOutline } from "react-icons/md";
 
 const Conta = () => {
   return (
@@ -9,19 +10,34 @@ const Conta = () => {
       <div className={styles.bloco}>
         <p className={styles.minhaConta}>Minha Conta</p>
         <div className={styles.components}>
-          <div className={styles.fotoPerfil}>
-            <FaCircleUser className={styles.iconUser} />
+          <div className={styles.fotoEBtn}>
+            <div className={styles.fotoPerfil}>
+              <FaCircleUser className={styles.iconUser} />
+            </div>
+            <button className={`${styles.button} ${styles.btnFoto}`}>
+              Alterar Foto
+            </button>
           </div>
-          <div className={styles.escritas}>
-            <p>Nome</p>
-            <input type="text" />
-            <p>E-mail</p>
-            <p></p>
+          <div className={styles.blocoDois}>
+            <div className={styles.escritas}>
+              <label>Nome</label>
+              <div><input type="text" /> <MdModeEditOutline/></div>
+              <label>E-mail</label>
+              <p>Kinzlindo123456@gmail.com</p>
+            </div>
+            <div className={styles.buttons}>
+              <button className={`${styles.button} ${styles.btnFoto}`}>
+                Alterar Senha
+              </button>
+              <button className={`${styles.button} ${styles.btnSalvar}`}>
+                Alterar Foto
+              </button>
+            </div>
           </div>
         </div>
-        <div>
-          <button>s</button>
-        </div>
+        <button className={`${styles.button} ${styles.btnSair}`}>
+          <IoExitOutline /> Sair
+        </button>
       </div>
     </main>
   );
