fb8472afa7544acec7f01ce7d8a72f9708f544a0
Guilherme Lins <guilhermelins978@gmail.com>
2024-01-19 17:17:33 -0300

    salvar

--- src/Pages/Conta/Conta.jsx
+++ src/Pages/Conta/Conta.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import styles from './index.module.css'
+import React from "react";
+import styles from "./index.module.css";
+import { FaUserCircle } from "react-icons/fa";
+import { FaCircleUser, FaRegCircleUser } from "react-icons/fa6";
 
 const Conta = () => {
   return (
-    <div className={styles.main}>Conta</div>
-  )
-}
+    <main className={styles.main}>
+      <div className={styles.bloco}>
+        <p className={styles.minhaConta}>Minha Conta</p>
+        <div className={styles.components}>
+          <div className={styles.fotoPerfil}>
+            <FaCircleUser className={styles.iconUser} />
+          </div>
+          <div className={styles.escritas}>
+            <p>Nome</p>
+            <input type="text" />
+            <p>E-mail</p>
+            <p></p>
+          </div>
+        </div>
+        <div>
+          <button>s</button>
+        </div>
+      </div>
+    </main>
+  );
+};
 
-export default Conta
+export default Conta;
