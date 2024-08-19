a611cd260bef9321a4027a20754e0a9abdaf490c
Guilherme Lins <guilhermelins978@gmail.com>
2024-08-19 18:17:05 -0300

    Adicionei conteúdo à pagina contato

--- src/Pages/Contato/index.jsx
+++ src/Pages/Contato/index.jsx
@@ -1,9 +1,38 @@
 import styles from "./index.module.css";
+import logoWpp from "../../img/logo-whatsapp.png"
+import logoInsta from "../../img/logo-insta.png"
+import emailLogo from "../../img/Email-logo.png"
+import logoLinkedin from "../../img/logo-linkedin.png"
 
 export default function Contato() {
   return (
     <main className={styles.content}>
-      <h1 className={styles.h1}>ENTRE EM CONTATO CONOSCO</h1>
+      <div className={styles.bloco}>
+        <h1 className={styles.h1}>ENTRE EM CONTATO CONOSCO</h1>
+        <div className={styles.buttons}>
+          <a className={styles.button}>
+            <img src={logoWpp} alt="" className={styles.logo}/>
+            <h2 className={styles.logoName}>Whatsapp</h2>
+            <p className={styles.paragraf}>(85) 98807-3262</p>
+          </a>
+          <a href="https://www.instagram.com/guillins15/" target="blank" className={styles.button}>
+            <img src={logoInsta} alt="" className={styles.logo}/>
+            <h2 className={styles.logoName}>Instagram</h2>
+            <p className={styles.paragraf}>@guillins15</p>
+          </a>
+          <a className={styles.button}>
+            <img src={emailLogo} alt="" className={styles.logo}/>
+            <h2 className={styles.logoName}>Email</h2>
+            <p className={styles.paragraf}>contato@dracomant.com</p>
+          </a>
+          <a href="https://www.linkedin.com/in/guilherme-lins-ab2ab3255/" 
+          target="blank" className={styles.button}>
+            <img src={logoLinkedin} alt="" className={styles.logo}/>
+            <h2 className={styles.logoName}>linkedin</h2>
+            <p className={styles.paragraf}>Guilherme Lins</p>
+          </a>
+        </div>
+      </div>
     </main>
   );
 }
