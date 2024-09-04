2a4962ebd9334e8e0d80f2f264ea1ea733b41e3f
Guilherme Lins <guilhermelins978@gmail.com>
2024-09-04 17:45:06 -0300

    fiz animação de brilho no footer

--- src/Components/Desenvolvimento/index.jsx
+++ src/Components/Desenvolvimento/index.jsx
@@ -4,7 +4,7 @@ import styles from "./index.module.css";
 export default function Desenvolvimento() {
   return (
     <div className={styles.content}>
-      <h1 className={styles.h1}>Em Desenvolvimento</h1>
+      <h1 className={styles.h1}>Em Desenvolvimento . . .</h1>
       <Loading/>
     </div>
   );
--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,9 +1,12 @@
 import styles from "./index.module.css";
 
 export default function Footer() {
+  const today = new Date()
+  const year = today.getFullYear()
+
   return (
     <footer className={styles.footer}>
-      <p>Dracomant &copy; 2023</p>
+      <p>Dracomant &copy; 2023 - {year}</p>
     </footer>
   );
 }
