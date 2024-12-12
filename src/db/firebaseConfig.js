885315a44e30b21664b9e26509bfd5254b4877e4
Guilherme Lins <guilhermelins978@gmail.com>
2024-12-12 15:05:58 -0300

    Consegui ocultar as coisas da minha api do firebase

--- src/db/firebaseConfig.js
+++ src/db/firebaseConfig.js
@@ -1,10 +1,13 @@
 
 import { initializeApp } from "firebase/app";
+const apiKey = import.meta.env.VITE_API_KEY; // Para Vite
+const authDomain = import.meta.env.VITE_AUTH_DOMAIN;
+const projectId = import.meta.env.VITE_PROJECT_ID;
 
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: "dracomant-abdd6.firebaseapp.com",
-  projectId: "dracomant-abdd6",
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
   storageBucket: "dracomant-abdd6.firebasestorage.app",
   messagingSenderId: "897453808489",
   appId: "1:897453808489:web:4e15c04a5d8be1ef387bf8",
